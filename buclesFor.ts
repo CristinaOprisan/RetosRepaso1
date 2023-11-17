53a64c846273e8626411e54db28c058b327a6d4d
Cristina Oprisan <oprisan.cristina29@gmail.com>
2023-11-17 19:35:34 +0100

    Up rama repaso1

---
+++ buclesFor.ts
@@ -0,0 +1,72 @@
+// Retos de Bucles For
+
+// 1. Guardar estos retos en un nuevo fichero. Ej: buclesFor.ts
+
+// 2. Realizar una función que imprima los números impares existentes hasta el número
+// indicado como parámetro de entrada.
+// La cabecera de la función tendrá el siguiente aspecto: function evenNumbers(num)
+
+function evenNumbers(num: number): void {
+    for (let i = 1; i <= num; i+=2){
+        console.log(i);   
+    }
+}
+evenNumbers(11)
+
+
+
+// 3. Realizar una función que como parámetro de entrada reciba un array y como salida
+// devuelva el array revertido. No se puede utilizar el método revert de la clase array
+// La cabecera de la función tendrá el siguiente aspecto: function myRevert(myArr)
+
+function myRevert(myArr: number[]): void{
+
+}
+
+
+
+// 4. Realizar una función que reciba como parámetro un array de strings que contenga
+// nombres de colores y te imprima en cada caso si el color está en el arcoíris o no.
+// La cabecera de la función tendrá el siguiente aspecto: function isRainbow(colors)
+
+function isRainbow(colors: string[]): void {
+    const rainbowColors: string[] = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+    
+    for (let x = 0; x < colors.length; x++) {
+        let color = colors[x];
+        let lowerCaseColor = color.toLocaleLowerCase()
+        let isRainbowColor = rainbowColors.includes(color)
+        
+        if (rainbowColors.includes(lowerCaseColor)) {
+            console.log(`${color} está en el arcoíris.`);
+        } else {
+            console.log(`${color} no está en el arcoíris.`);
+        }
+    }
+}
+
+let colors = ['red', 'pink'];
+console.log(isRainbow(colors));
+
+
+
+// 5. Realizar una función que te devuelva la suma del numero de caracteres de las palabras
+// almacenadas en un array.
+// La cabecera de la función tendrá el siguiente aspecto: function add(myWords)
+
+export function add(myWords: string[]){
+    let sumCharacters = 0;
+    for (let y = 0; y < myWords.length; y++) {
+        if (typeof myWords[y] === 'string'){
+            sumCharacters += myWords[y].length;
+        }   
+    }
+    return sumCharacters
+}
+let wordsArray = ['contar', 'caracteres'];
+console.log(add(wordsArray));
+
+
+
+
+// 6. Subir los cambios a GitHub.
---
+++ buclesWhile.js
@@ -0,0 +1,33 @@
+// Retos de Bucles While
+// 1. Guardar estos retos en un nuevo fichero. Ej: buclesWhile.ts
+// 2. Realizar una función que te devuelva si existe un numero par en el array de números que
+// introduces como parámetro de entrada.
+// La cabecera de la función tendrá el siguiente aspecto: function hasEven(myNums)
+export function hasEven(myNums) {
+    let i = 0;
+    while (i <= myNums.length) {
+        if (myNums[i] % 2 === 0) {
+            return true;
+        }
+        i++;
+    }
+    return false;
+}
+let numsArray = [7, 60, 44, 31];
+console.log(hasEven(numsArray));
+// 3. Realizar una función que reciba un array de nombres y te devuelva verdadero si y solo si
+// todos los nombres empiezan por M.
+// La cabecera de la función tendrá el siguiente aspecto: function startWithM(myNames)
+function startWithM(myNames) {
+    let i = 0;
+    while (i <= myNames.length) {
+        if (myNames[i] && myNames[i][0].toLocaleLowerCase() !== 'm') {
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
+let namesArray = ['Marlen', 'Maria', 'Mario'];
+console.log(startWithM(namesArray));
+// 4. Subir los cambios a GitHub.
